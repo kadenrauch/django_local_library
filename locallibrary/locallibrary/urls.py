fceabf7d887c7934bf9f874268bb45f3319e9ac4
Kaden Rauch <kadenmrauch@gmail.com>
2025-03-25 10:14:42 -0400

    Django-Ninja

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -19,9 +19,12 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from django.views.generic import RedirectView
+from catalog.api import api
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", api.urls),
     path('catalog/', include('catalog.urls')),
     path('', RedirectView.as_view(url='catalog/', permanent=True)),
     path('accounts/', include('django.contrib.auth.urls')),
